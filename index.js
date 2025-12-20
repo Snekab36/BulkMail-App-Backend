df6a575b12db691e4b443f928b7293681f9495cf
snekab36 <bsneka36@gmail.com>
2025-12-20 11:27:38 +0530

    Fix backend deployment config

--- index.js
+++ index.js
@@ -70,8 +70,9 @@ app.post("/api/sendemail", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
